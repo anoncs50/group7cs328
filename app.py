4700ca302723067aac41269e5a6d6e08d6041c02
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-12-15 17:38:04 -0500

    ljn

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import base64
 import math
 from flask_session import Session
-import time
+from time import time
 import psycopg2
 import os
 
@@ -60,65 +60,63 @@ def data():
     DATABASE_URL = os.environ.get('DATABASE_URL')
     con = psycopg2.connect(DATABASE_URL)
     cur = con.cursor()
-    if True:
-        u = request.args.get('u')
-        try:
-            cur.execute("""
-            SELECT *  FROM users WHERE username = %s;        
-            """, (u,))
-        except Exception:
-            cur.execute("""
-            CREATE TABLE users (
-               username TEXT NOT NULL PRIMARY KEY,
-               time INT[],
-               accx INT[],
-               accy INT[],
-               accz INT[]
-            );
-            """)
-            cur.execute("""
-            SELECT *  FROM users WHERE username = %s;        
-            """, (u,))
-        user = cur.fetchone()
-        if not user is None:
-            user[1].append(time.now())
-            user[2].append(int(request.args.get('accx')))
-            user[3].append(int(request.args.get('accy')))
-            user[4].append(int(request.args.get('accz')))
-            accx = np.array(user[2])
-            accy = np.array(user[3])
-            accz = np.array(user[4])
-            tim= np.array(user[1])
-            tim = tim - tim[0]
-            cur.execute(""" UPDATE users
-                SET time = %s
-                WHERE username = %s""", (tim,), (u,))
-            cur.execute(""" UPDATE users
-                SET accx = %s
-                WHERE username = %s""", (accx,), (u,))
-            cur.execute(""" UPDATE users
-                SET accy = %s
-                WHERE username = %s""", (accy,), (u,))
-            cur.execute(""" UPDATE users
-                SET accz = %s
-                WHERE username = %s""", (accz), (u,))
-            plt.plot(tim, accx,"-r", tim, accy, "-g", tim, accz, "-b")
-            plt.xlabel("Time (s)")
-            plt.ylabel("Acceleration (m/s/s)")
-            plt.legend(['x-axis', 'y-axis', 'z-axis'])
-            plotData=[]
-            fig = BytesIO()
-            plt.savefig(fig, format='png')
-            fig.seek(0)
-            buffer = b''.join(fig)
-            b2 = base64.b64encode(buffer)
-            figDec =b2.decode('utf-8')
-            plotData.append(figDec)
-            return  '<img src="data:image/png;base64,' + plotData[0] + '" alt = "accelerometer data" style="width: 20%" >'
-        con.commit()
-        con.close()
-        return "<p>user not found</p>"
-    return "<p>empty</p>"
+    u = request.args.get('u')
+    try:
+        cur.execute("""
+        SELECT *  FROM users WHERE username = %s;        
+        """, (u,))
+    except Exception:
+        cur.execute("""
+        CREATE TABLE users (
+            username TEXT NOT NULL PRIMARY KEY,
+            time INT[],
+            accx INT[],
+            accy INT[],
+            accz INT[]
+        );
+        """)
+        cur.execute("""
+        SELECT *  FROM users WHERE username = %s;        
+        """, (u,))
+    user = cur.fetchone()
+    if not user is None:
+        user[1].append(time.now())
+        user[2].append(int(request.args.get('accx')))
+        user[3].append(int(request.args.get('accy')))
+        user[4].append(int(request.args.get('accz')))
+        accx = np.array(user[2])
+        accy = np.array(user[3])
+        accz = np.array(user[4])
+        tim= np.array(user[1])
+        tim = tim - tim[0]
+        cur.execute(""" UPDATE users
+            SET time = %s
+            WHERE username = %s""", (tim,), (u,))
+        cur.execute(""" UPDATE users
+            SET accx = %s
+            WHERE username = %s""", (accx,), (u,))
+        cur.execute(""" UPDATE users
+            SET accy = %s
+            WHERE username = %s""", (accy,), (u,))
+        cur.execute(""" UPDATE users
+            SET accz = %s
+            WHERE username = %s""", (accz), (u,))
+        plt.plot(tim, accx,"-r", tim, accy, "-g", tim, accz, "-b")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Acceleration (m/s/s)")
+        plt.legend(['x-axis', 'y-axis', 'z-axis'])
+        plotData=[]
+        fig = BytesIO()
+        plt.savefig(fig, format='png')
+        fig.seek(0)
+        buffer = b''.join(fig)
+        b2 = base64.b64encode(buffer)
+        figDec =b2.decode('utf-8')
+        plotData.append(figDec)
+        return  '<img src="data:image/png;base64,' + plotData[0] + '" alt = "accelerometer data" style="width: 20%" >'
+    con.commit()
+    con.close()
+    return "<p>user not found</p>"
 # runs our app using Flask
 if __name__ == "__main__":
     app.run(debug = True)
