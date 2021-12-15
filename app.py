e4c7b4b7751f54443325213e6ebce1d9eeb5eea8
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-12-14 23:17:06 -0500

    fa

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def login():
             """, (session['u'], [], [], [], []))
         con.commit()
         con.close()
-        return '<p>'+session['u']+'</p>'
+        return '<p>Hello, '+session['u']+'!</p>'
 
 @app.route('/clear')
 def clear():
@@ -58,38 +58,54 @@ def clear():
 #get data
 @app.route('/data')
 def data():
-    if not 'accx' in session or not 'accy' in session or not 'accz' or not 'time' in session:
-       session['accx'] = []
-       session['accy'] = []
-       session['accz'] = []
-       session['time'] = []
-    accx = session['accx']
-    accy = session['accy']
-    accz = session['accz']
-    tim = session['time']
-    accx.append(int(request.args.get('x')))
-    accy.append(int(request.args.get('y')))
-    accz.append(int(request.args.get('z')))
-    tim.append(time.time())
-    session['accx']=accx
-    session['accy']=accy
-    session['accz']=accz
-    session['time']=tim
-    tim = np.array(tim)
-    tim=tim-tim[0]
-    plt.plot(tim, accx,"-r", tim, accy, "-g", tim, accz, "-b")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Acceleration (m/s/s)")
-    plt.legend(['x-axis', 'y-axis', 'z-axis'])
-    plotData=[]
-    fig = BytesIO()
-    plt.savefig(fig, format='png')
-    fig.seek(0)
-    buffer = b''.join(fig)
-    b2 = base64.b64encode(buffer)
-    figDec =b2.decode('utf-8')
-    plotData.append(figDec)
-    return  '<img src="data:image/png;base64,' + plotData[0] + '" alt = "accelerometer data" style="width: 20%" >'
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    con = psycopg2.connect(DATABASE_URL)
+    cur = con.cursor()
+    if session['u'] and request.args.get('accx') and request.args.get('accy') and request.args.get('accz'):
+        try:
+            cur.execute("""
+            SELECT *  FROM users WHERE username = %s;        
+            """, (session['u'],))
+        except Exception:
+            cur.execute("""
+            CREATE TABLE users (
+               username TEXT NOT NULL PRIMARY KEY,
+               time INT[],
+               accx INT[],
+               accy INT[],
+               accz INT[]
+            );
+            """)
+            cur.execute("""
+            SELECT *  FROM users WHERE username = %s;        
+            """, (session['u'],))
+        user = cur.fetchone()
+        if user:
+            user[1].append(time.now())
+            user[2].append(int(request.args.get('accx')))
+            user[3].append(int(request.args.get('accy')))
+            user[4].append(int(request.args.get('accz')))
+            accx = np.array(user[2])
+            accy = np.array(user[3])
+            accz = np.array(user[4])
+            tim= np.array(user[1])
+            tim = tim - tim[0]
+            plt.plot(tim, accx,"-r", tim, accy, "-g", tim, accz, "-b")
+            plt.xlabel("Time (s)")
+            plt.ylabel("Acceleration (m/s/s)")
+            plt.legend(['x-axis', 'y-axis', 'z-axis'])
+            plotData=[]
+            fig = BytesIO()
+            plt.savefig(fig, format='png')
+            fig.seek(0)
+            buffer = b''.join(fig)
+            b2 = base64.b64encode(buffer)
+            figDec =b2.decode('utf-8')
+            plotData.append(figDec)
+            return  '<img src="data:image/png;base64,' + plotData[0] + '" alt = "accelerometer data" style="width: 20%" >'
+        con.commit()
+        con.close()
+        return "<br>"
 # runs our app using Flask
 if __name__ == "__main__":
     app.run(debug = True)
