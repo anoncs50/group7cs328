b2457be408b5e28d2ade8f68756379a24d1f3d65
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-12-14 20:09:04 -0500

    ads
    sda

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ import base64
 import math
 from flask_session import Session
 import time
+import psycopg2
+
+
+DATABASE_URL = os.environ.get(‘DATABASE_URL’)
+con = psycopg2.connect(DATABASE_URL)
+cur = con.cursor()
+try:
+    cur.execute("""
+        CREATE TABLE users (
+           username TEXT NOT NULL PRIMARY KEY,
+           time INT[],
+           accx INT[],
+           accy INT[],
+           accz INT[]
+        );
+    """)
+except Exception:
+    pass
 # the app we use to manage the routes and run the app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "gotti"
@@ -14,12 +32,26 @@ app.config['SECRET_KEY'] = "gotti"
 @app.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
-        return render_template("home.html")    
+        return render_template("home.html")  
+@app.route('/login')
+def login():
+    if request.args.get('u'):
+        session['u'] = request.args.get('u')
+        cur.execute("""
+        SELECT *  FROM users WHERE username = %s;        
+        """, (session['u'],))
+        user = cur.fetchone()
+        if user is None:
+            cur.execute("""
+            INSERT INTO users(username, time, accx, accy, accz)
+            VALUES (%s, %s, %s, %s, %s)
+            """, (session['u'], [], [], [], []))
+        con.commit()
+        return '<br>'
 #get data
 @app.route('/data')
 def data():
-
-    if request.args.get('clear') or not 'accx' in session or not 'accy' in session or not 'accz' or not 'time' in session:
+    if not 'accx' in session or not 'accy' in session or not 'accz' or not 'time' in session:
        session['accx'] = []
        session['accy'] = []
        session['accz'] = []
@@ -28,17 +60,16 @@ def data():
     accy = session['accy']
     accz = session['accz']
     tim = session['time']
-    if not request.args.get('clear'):
-        accx.append(int(request.args.get('x')))
-        accy.append(int(request.args.get('y')))
-        accz.append(int(request.args.get('z')))
-        tim.append(time.time())
-        session['accx']=accx
-        session['accy']=accy
-        session['accz']=accz
-        session['time']=tim
-        tim = np.array(tim)
-        tim=tim-tim[0]
+    accx.append(int(request.args.get('x')))
+    accy.append(int(request.args.get('y')))
+    accz.append(int(request.args.get('z')))
+    tim.append(time.time())
+    session['accx']=accx
+    session['accy']=accy
+    session['accz']=accz
+    session['time']=tim
+    tim = np.array(tim)
+    tim=tim-tim[0]
     plt.plot(tim, accx,"-r", tim, accy, "-g", tim, accz, "-b")
     plt.xlabel("Time (s)")
     plt.ylabel("Acceleration (m/s/s)")
